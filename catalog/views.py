0cc1dc1bd7b20ed1270c5d266faf9485e0fb6a89
serebriakovanton <cu-hawk@yandex.ru>
2024-06-11 22:36:58 +0700

    add controllers

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product, Category
 
 
 def home(request):
@@ -10,7 +11,15 @@ def contacts(request):
 
 
 def product(request):
-    return render(request, "product.html")
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, "product.html", context)
+
+
+def category(request):
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, "category.html", context)
 
 
 def base(request):
@@ -18,3 +27,17 @@ def base(request):
         'base_templates': 'base'
     }
     return render(request, 'base.html', context)
+
+
+def product_review(request, pk):
+    product_ = get_object_or_404(Product, pk=pk)
+    context = {'product': product_}
+    return render(request, "product_review.html", context)
+
+
+def category_review(request):
+    context = {'category_review': 'category_review'}
+    return render(request, "category_review.html", context)
+
+
+
