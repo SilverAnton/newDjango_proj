3f6bf24ae0131c06204cf303cbf70ad5afcd0839
serebriakovanton <cu-hawk@yandex.ru>
2024-06-19 14:24:33 +0700

    update FBV controllers by CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product, base, product_review, category, category_review
+from catalog.views import HomePageView, BasePageView, ContactsPageView, ProductListView, ProductDetailView, CategoryListView, CategoryDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="index"),
-    path("contacts/", contacts, name="contacts"),
-    path("product/", product, name="product"),
-    path("category/", category, name="category"),
-    path("base/", base, name="base"),
-    path("product/<int:pk>/", product_review, name="product_review"),
-    path("category_review/", category_review, name="category_review"),
+    path("", HomePageView.as_view(), name="index"),
+    path("contacts/", ContactsPageView.as_view(), name="contacts"),
+    path("category/catalog", ProductListView.as_view(), name="product"),
+    path("category/catalog/", CategoryListView.as_view(), name="category"),
+    path("base/", BasePageView.as_view(), name="base"),
+    path("product_review/catalog/<int:pk>/", ProductDetailView.as_view(), name="product_review"),
+    path("category_review/catalog/<int:pk>/", CategoryDetailView.as_view(), name="category_review"),
 
 ]
