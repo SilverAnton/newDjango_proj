0e1206598ebb13202d40b429d1417d4ad7234296
serebriakovanton <cu-hawk@yandex.ru>
2024-07-14 16:19:34 +0700

    update file

--- catalog/forms.py
+++ catalog/forms.py
@@ -39,3 +39,9 @@ class VersionForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Version
         fields = '__all__'
+
+
+class ProductModeratorForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['is_published', 'description', 'category']
